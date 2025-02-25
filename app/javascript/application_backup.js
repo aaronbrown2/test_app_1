ee5205f07f7eb5fadf273459b4bb89d4a57d79eb
Aaron Brown <aaronbrown.mpls@gmail.com>
2025-02-25 10:19:19 -0500

    finish articles layout and functionality

--- app/javascript/application_backup.js
+++ app/javascript/application_backup.js
@@ -1,3 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@rails/ujs";
-console.log("Rails UJS Loaded");  // Debugging
+import "@hotwired/turbo-rails"
+import "controllers"
